63fbe0d6099e86643616ee96adf37d3a65cb46c6
Kirill Safelkin <k.safelkin@mail.ru>
2023-01-06 22:35:02 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import telebot
+bot = telebot.TeleBot('5729532686:AAGXpQ26h6RTTcciCEqu4MTbVpDFXA5NVwk')
+
+name = ''
+surname = ''
+age = 0
+@bot.message_handler(content_types=['text'])
+def start(message):
+    if message.text == '/reg':
+        bot.send_message(message.from_user.id, "Привет! Это бот великого и ужасного Кирилла Сафёлкина.\nНапиши как тебя зовут?")
+        bot.register_next_step_handler(message, get_name)
+    else:
+        bot.send_message(message.from_user.id, 'Напиши /reg')
+
+def get_name(message):
+    global name
+    name = message.text
+    bot.send_message(message.from_user.id, 'Какая у тебя фамилия?')
+    bot.register_next_step_handler(message, get_surname)
+
+def get_surname(message):
+    global surname
+    surname = message.text
+    bot.send_message(message.from_user.id, 'Сколько тебе лет?')
+    bot.register_next_step_handler(message, get_age)
+
+def get_age(message):
+    global age
+    while age == 0:
+        try:
+            age = int(message.text)
+        except Exception:
+            bot.send_message(message.from_user.id, 'Цифрами, пожалуйста')
+    bot.send_message(message.from_user.id, 'Тебе '+str(age)+' лет, тебя зовут '+name+' '+surname+'\nВывод: ты вонючий слизняк')
+
+bot.infinity_polling()
